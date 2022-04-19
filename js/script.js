bbc37843cf68c247f8e8769ef568e164f9e64f08
busraozata <busraozata1998@gmail.com>
2022-04-19 18:30:27 +0300

    fifth commit

--- js/script.js
+++ js/script.js
@@ -27,3 +27,20 @@ $(toggleViewGalery).click(function () {
     $('.galery-view').hide();
     $('.toggle-view-galery').hide();
 });
+
+
+var btn = $('#back-to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
